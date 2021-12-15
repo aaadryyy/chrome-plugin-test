daa9515e6543780c051c52306f91dd3deff92c56
aaadryyy <adrienquillet@gmail.com>
2021-12-16 00:03:53 +0100

    ✨feat: change curren page colour background

---
+++ background.js
@@ -0,0 +1,6 @@
+let color = "#353535";
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log("Default background color set to %cgreen", `color: ${color}`);
+});
